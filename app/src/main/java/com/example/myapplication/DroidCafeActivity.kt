f3fc5d827282e2e8fdf1a4e682ab9f6495663f25
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-05-15 23:08:01 -0300

    committed by Abner

--- app/src/main/java/com/example/myapplication/DroidCafeActivity.kt
+++ app/src/main/java/com/example/myapplication/DroidCafeActivity.kt
@@ -1,35 +1,54 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.DrawableRes
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DroidCafeActivity : AppCompatActivity() {
+    @DrawableRes
+    private var imageId: Int = 0;
+    private var imageName: String = "";
+    private val KEY_IMAGEM: String = "imagem";
+    private val KEY_IMAGEM_NAME: String = "imagem_name";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_droid_cafe)
 
-        val iv_donuts: ImageView = findViewById(R.id.iv_donuts);
-        val iv_ice_cream: ImageView = findViewById(R.id.iv_ice_cream);
-        val iv_fro_yo: ImageView = findViewById(R.id.iv_fro_yo);
-
-        val btnf_cart: ImageView = findViewById(R.id.btnf_cart);
+        val iv_donuts = findViewById<ImageView>(R.id.iv_donuts);
+        val iv_ice_cream = findViewById<ImageView>(R.id.iv_ice_cream);
+        val iv_fro_yo = findViewById<ImageView>(R.id.iv_fro_yo);
+        val btnf_cart = findViewById<FloatingActionButton>(R.id.btnf_cart);
 
         iv_donuts.setOnClickListener() {
-            Toast.makeText(this, "donut", Toast.LENGTH_LONG).show();
+            imageId = R.drawable.donut_circle;
+            imageName = getString(R.string.donuts_text);
+            Toast.makeText(this, imageName, Toast.LENGTH_LONG).show();
         }
 
         iv_ice_cream.setOnClickListener() {
-            Toast.makeText(this, "ice cream", Toast.LENGTH_LONG).show();
+            imageId = R.drawable.icecream_circle;
+            imageName = getString(R.string.ice_cream_text);
+            Toast.makeText(this, imageName, Toast.LENGTH_LONG).show();
         }
 
         iv_fro_yo.setOnClickListener() {
-            Toast.makeText(this, "fro yo", Toast.LENGTH_LONG).show();
+            imageId = R.drawable.froyo_circle;
+            imageName = getString(R.string.fro_yo_text);
+            Toast.makeText(this, imageName, Toast.LENGTH_LONG).show();
         }
 
         btnf_cart.setOnClickListener() {
-            Toast.makeText(this, "btnf_cart", Toast.LENGTH_LONG).show();
+            val openScreenIntent = Intent(this, SecontActivity::class.java).also {
+                it.putExtra("KEY_IMAGEM", imageId);
+                it.putExtra("KEY_IMAGEM_NAME", imageName);
+                startActivity(it);
+            };
+
+
         }
     }
 }
--- app/src/main/java/com/example/myapplication/SecontActivity.kt
+++ app/src/main/java/com/example/myapplication/SecontActivity.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class SecontActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_secont)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secont);
+
+        val imageId = intent.getIntExtra("KEY_IMAGEM", R.drawable.donut_circle);
+        val imageName = intent.getStringExtra("KEY_IMAGEM_NAME").orEmpty();
+
+        val ivImage: ImageView = findViewById(R.id.iv_image_response);
+        val tvImageName: TextView = findViewById(R.id.tv_texte_image_response);
+
+        ivImage.setImageResource(imageId);
+        tvImageName.text = imageName;
     }
 }
